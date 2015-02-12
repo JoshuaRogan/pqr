1852c5c42d61b813a9209ae1c5b2f2def323d9ab
JoshuaRogan <jjjr1122@gmail.com>
2015-02-12 01:54:49 -0500

    Update Splash Config

--- app/config/splash_config.php
+++ app/config/splash_config.php
@@ -2,13 +2,13 @@
 
 class config extends global_config{
 
-  public static $pageTitle 			= "PQR | Home";
+  public static $pageTitle 				= "PQR | Home";
   public static $pageDescription 		= "Splash";
 
-  public static $stylesheets 			= array("styles/flat-ui.css", "styles/splash.css");
+  public static $stylesheets 			= array("//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css", "styles/flat-ui.css", "styles/splash.css");
   public static $javascript 			= array();
 
-  public static $header				= "header";
+  public static $header					= "header";
   public static $footer 				= "footer";
 
 }
