fc9a36606b01e0e3abe18327a9710e32d5e027df
Ritwik Gupta <ritwikg2004@live.com>
2015-02-12 12:12:48 -0500

    Added link to footer

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,53 +1,53 @@
-<?php 
+<?php
 /**
  *	General Footer page
  *
  */
 
-	
+
 ?>
 
-<footer> 
+<footer>
 	<div class="container-fluid ">
 
 
 		<div class="row">
-			<div class="col-md-3"> 
+			<div class="col-md-3">
 				<h3> Credits: </h3>
 				<p>
 					Hutchison group at the University of Pittsburgh<br/>
 					Lambrecht group at the University of Pittsburgh<br/>
 				 </p>
-				<p> Application created by <a href="http://joshuarogan.com" target="_blank">Josh Rogan</a> &amp; <a href="#"> Ritwik Gupta </a></p>
+				<p> Application created by <a href="http://joshuarogan.com" target="_blank">Josh Rogan</a> &amp; <a href="http://ritwikgupta.me"> Ritwik Gupta </a></p>
 				<p>Copyright © 2015</p>
 			</div>
 
-			<div class="col-md-6"> 
+			<div class="col-md-6">
 				<h3> About Us </h3>
 				<p> Fanny pack hashtag wolf, four dollar toast pork belly tousled cornhole tattooed flannel viral. Artisan chia keytar lumbersexual four loko Portland bitters Banksy tattooed locavore, umami you probably haven't heard of them kogi tilde occupy. Ennui seitan locavore, master cleanse cray church-key fingerstache Odd Future four dollar toast pug food truck actually ugh. Ennui pug salvia biodiesel actually. Heirloom VHS DIY meh. Hella butcher photo booth fap, jean shorts kogi swag VHS Portland 3 wolf moon direct trade viral fashion axe. Meditation High Life tofu organic.</p>
 
 
 				<h4>Fanny pack hashtag wolf</h4>
-				<ul class="list-unstyled"> 
+				<ul class="list-unstyled">
 					<li> Street Address </li>
 					<li> Pittsburgh, PA 15261 </li>
 					<li> ###-###-#### </li>
 				</ul>
-			
 
-				
+
+
 			</div>
 
-			<div class="col-md-3"> 
+			<div class="col-md-3">
 				<h3> Report a Problem </h3>
 				<p> Found a problem? <a href="/report-problem">Report it here!</a> </p>
-				<img class="img-responsive right-block" alt="footer-logo" src="img/footer-logo.png" width="80px"> 
+				<img class="img-responsive right-block" alt="footer-logo" src="img/footer-logo.png" width="80px">
 			</div>
 
 		</div>
 
-		<div class="row"> 
-			<ul class="list-unstyled list-inline text-center"> 
+		<div class="row">
+			<ul class="list-unstyled list-inline text-center">
 					<li> <a href="#" target="_blank"><i class="fa fa-facebook fa-lg" ></i></a> </li>
 					<li> <a href="#" target="_blank"><i class="fa fa-twitter fa-lg" ></i></a> </li>
 					<li> <a href="#" target="_blank"><i class="fa fa-linkedin fa-lg"></i></a> </li>
@@ -61,6 +61,6 @@
 				<li><a href="#">News</a></li>
 				<li><a href="#">Acknowledgments/a></li>
 			</ul>
-		</div> 
-	</div> 
+		</div>
+	</div>
 </footer>
